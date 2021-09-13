fbb9162ae8327df91c4eed4815bdb27da92bc88c
DogukanSAVA <dogukan98sv@hotmail.com>
2021-09-13 13:34:18 +0300

    navbar

---
+++ navbar/.history/app_20210913124447.js
@@ -0,0 +1,7 @@
+onst navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+
+  links.classList.toggle("show-links");
+});
